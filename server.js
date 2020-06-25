cf0fe6f5c2e00e2e39078cccbe9cd6681823f333
ahsantawil <ahsan.tawil07@gmail.com>
2020-06-25 22:35:02 +0700

    arrow function and console.log node

--- server.js
+++ server.js
@@ -2,14 +2,15 @@
 
 const express   = require('express')
 const app       = express()
+const port      = 3000
 
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => 
     res.send('Halo Dunia')
-})
+)
 
 app.get('/users/:name', function(req, res){
     res.send(`Halaman user ${req.params.name}`)
 })
 
-app.listen(3000);
+app.listen(port, ()=> console.log(`Buka url di local 127.0.0.1:${port}`));
